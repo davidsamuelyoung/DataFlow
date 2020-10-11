3dc050d7fb6da6a2bb79adb8e81fd45d1ef72b69
David Young <youngdavidsamuel@gmail.com>
2020-10-11 09:52:38 +0200

    Initial Commit

---
+++ DataFlow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DataFlow
+//
+//  Created by David Young on 2020/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DataFlow/DataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowApp.swift
+//  DataFlow
+//
+//  Created by David Young on 2020/10/11.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
